01191a2f84d2ff58d1f85d851dbaa34765729023
GabrielaRBLemos <gabrielarblemos@gmail.com>
2025-12-25 18:46:14 -0300

    Added third variant

--- randomness/sky/sketch.js
+++ randomness/sky/sketch.js
@@ -9,6 +9,7 @@ const canvasHeight = window.innerHeight;
 let luminance;
 let hue;
 let offset = 0.0;
+let speed=0.0;
 
 function preload() {
    music = loadSound("/assets/piano-438549.mp3");
@@ -62,9 +63,10 @@ function draw() {
         for (let y = 0; y < height; y++) {
             //luminance = map(noise(xoff, yoff), 0, 1, 0, 100);
             // hue = map(noise(xoff, yoff), 0, 1, 0, 360);
-            luminance = map(noise(xoff, yoff), 0, 1, 40, 110);// max over 10 from max limit of l to make pure white "the sun behind cloud" effect
-            hue = map(noise(xoff, yoff), 0, 1, 192, 220);
-            saturation = map(noise(xoff, yoff), 0, 1, 100, 80);
+            // luminance = map(noise(xoff, yoff), 0, 1, 40, 110);// max over 10 from max limit of l to make pure white "the sun behind cloud" effect
+            luminance = map(noise(xoff, yoff,speed), 0, 1, 40, 110);// max over 10 from max limit of l to make pure white "the sun behind cloud" effect
+            hue = map(noise(xoff, yoff, speed), 0, 1, 192, 220);
+            saturation = map(noise(xoff, yoff, speed), 0, 1, 100, 80);
             // pixelColor = color(hue,100,luminance);
             pixelColor = color(hue,saturation,luminance);
             set(x, y, pixelColor);
@@ -72,6 +74,7 @@ function draw() {
         }
         xoff += 0.01;
     }
+    speed+=0.02
     updatePixels();
 }
 
